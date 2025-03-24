6293a6f9e087884a83b49b5baf23706c158f3054
Michael Onyango <oriengoonyango@gmail.com>
2025-03-24 10:55:29 +0300

    My first commit

---
+++ footer.php
@@ -0,0 +1,115 @@
+ <!-- Contact Section -->
+ <section id="contact" class="contact section">
+
+<!-- Section Title -->
+<div class="container section-title" data-aos="fade-up">
+  <h2>Contact</h2>
+  <p>Contact us for a personalized event quote to bring your dream event to life</p>
+</div><!-- End Section Title -->
+
+<div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
+
+  <div class="row gy-4">
+
+    <div class="col-lg-5">
+      <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="200">
+        <i class="bi bi-geo-alt flex-shrink-0"></i>
+        <div>
+          <h3>Address</h3>
+          <p>Bondo kwach market along Kisumu-Usenge Highway</p>
+        </div>
+      </div><!-- End Info Item -->
+
+      <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
+        <i class="bi bi-telephone flex-shrink-0"></i>
+        <div>
+          <h3>Call Us</h3>
+          <p>0741171408</p>
+        </div>
+      </div><!-- End Info Item -->
+
+      <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="400">
+        <i class="bi bi-envelope flex-shrink-0"></i>
+        <div>
+          <h3>Email Us</h3>
+          <p>info@obyentertainment.com</p>
+        </div>
+      </div><!-- End Info Item -->
+
+    </div>
+
+    <div class="col-lg-7">
+      <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="500">
+        <div class="row gy-4">
+
+          <div class="col-md-6">
+            <input type="text" name="name" class="form-control" placeholder="Your Name" required="">
+          </div>
+
+          <div class="col-md-6 ">
+            <input type="email" class="form-control" name="email" placeholder="Your Email" required="">
+          </div>
+
+          <div class="col-md-12">
+            <input type="text" class="form-control" name="subject" placeholder="Subject" required="">
+          </div>
+
+          <div class="col-md-12">
+            <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
+          </div>
+
+          <div class="col-md-12 text-center">
+            <div class="loading">Loading</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Your message has been sent. Thank you!</div>
+
+            <button type="submit">Send Message</button>
+          </div>
+
+        </div>
+      </form>
+    </div><!-- End Contact Form -->
+
+  </div>
+
+</div>
+
+</section><!-- /Contact Section -->
+<footer id="footer" class="footer">
+
+<div class="container">
+  <div class="copyright text-center ">
+    <p>© <span>Copyright</span> <strong class="px-1 sitename">Oby Entertaiment</strong> <span>All Rights Reserved</span></p>
+  </div>
+  <div class="social-links d-flex justify-content-center">
+    <a href=""><i class="bi bi-twitter-x"></i></a>
+    <a href=""><i class="bi bi-facebook"></i></a>
+    <a href=""><i class="bi bi-instagram"></i></a>
+    <a href=""><i class="bi bi-linkedin"></i></a>
+  </div>
+  <div class="credits">
+  
+    Designed by <a href="#">Oritech Solutions</a>
+  </div>
+</div>
+
+</footer>
+
+<!-- Scroll Top -->
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Preloader -->
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+<!-- Main JS File -->
+<script src="assets/js/main.js"></script>
